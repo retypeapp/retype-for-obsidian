a57fd17ae7816a68a6616bfefed898db7f1f4470
Geoffrey McGill <geoff@object.net>
2026-03-19 17:13:50 -0600

    Add eslint/parser

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import tsparser from "@typescript-eslint/parser";
+import { defineConfig } from "eslint/config";
+import obsidianmd from "eslint-plugin-obsidianmd";
+
+export default defineConfig([
+    {
+        ignores: ["main.js", "node_modules/**"],
+    },
+    ...obsidianmd.configs.recommended,
+    {
+        files: ["src/**/*.ts"],
+        languageOptions: {
+            parser: tsparser,
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+        },
+        rules: {
+            "obsidianmd/ui/sentence-case": [
+                "warn",
+                {
+                    brands: ["Retype", "Retype CLI"],
+                    acronyms: ["URL"],
+                    enforceCamelCaseLower: true,
+                },
+            ],
+        },
+    },
+]);
